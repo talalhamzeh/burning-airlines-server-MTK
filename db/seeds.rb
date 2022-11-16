b0e98a6e8ed0bd6d85725c536e9819bd298a2e40
Michael Anderson <manderson3568@outlook.com.au>
2022-11-16 11:20:12 +1100

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Airplanes.destroy_all
+Airplane.destroy_all
 
-a1= Airplanes.create(:name => 'B737', :rows => '10', :columns => '6')
-a2= Airplanes.create(:name => 'B747', :rows => '10', :columns => '6')
-a3= Airplanes.create(:name => 'A320', :rows => '10', :columns => '6')
-
-
-Flights.destroy_all
-
-f1= Flights.create(:number => BA356, :destination => 'SYD', :departure=> "MEL", :date => Time.new(2022, 12, 15))
-f2= Flights.create(:number => BA678, :destination => 'BNE', :departure=> "CBR", :date => Time.new(2023, 1, 5))
-f3= Flights.create(:number => BA921, :destination => 'PER', :departure=> "SYD", :date => Time.new(2023, 2, 10))
-f4= Flights.create(:number => BA590, :destination => 'ADL', :departure=> "BNE", :date => Time.new(2023, 1, 15))
-f5= Flights.create(:number => BA728, :destination => 'HBA', :departure=> "MEL", :date => Time.new(2023, 1, 20))
+a1= Airplane.create(:name => 'B737', :rows => '10', :columns => '6')
+a2= Airplane.create(:name => 'B747', :rows => '10', :columns => '6')
+a3= Airplane.create(:name => 'A320', :rows => '10', :columns => '6')
 
 
-f1<<a1
-f2<<a1
-f3<<a1
-f4<<a1
-f5<<a1
+Flight.destroy_all
 
-f1<<a2
-f2<<a2
-f3<<a2
-f4<<a2
-f5<<a2
+f1= Flight.create(:number => 'BA356', :destination => 'SYD', :departure=> "MEL", :date => Time.new(2022, 12, 15))
+f2= Flight.create(:number => 'BA678', :destination => 'BNE', :departure=> "CBR", :date => Time.new(2023, 1, 5))
+f3= Flight.create(:number => 'BA921', :destination => 'PER', :departure=> "SYD", :date => Time.new(2023, 2, 10))
+f4= Flight.create(:number => 'BA590', :destination => 'ADL', :departure=> "BNE", :date => Time.new(2023, 1, 15))
+f5= Flight.create(:number => 'BA728', :destination => 'HBA', :departure=> "MEL", :date => Time.new(2023, 1, 20))
 
-f1<<a3
-f2<<a3
-f3<<a3
-f4<<a3
-f5<<a3
+a1.flights << f1 << f2
+a2.flights << f3 << f4
+a3.flights << f5
+
 
